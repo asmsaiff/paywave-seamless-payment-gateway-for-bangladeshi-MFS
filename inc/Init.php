6d614ad2b5a723422092fa38df6131daac176bae
asmsaiff <asmsaif15@gmail.com>
2024-08-26 12:52:06 +0600

    delete old files

--- inc/Init.php
+++
@@ -1,9 +0,0 @@
-<?php
-namespace PayWave;
-
-class Init {
-    public static function run() {
-        // Register the gateway
-        add_filter('woocommerce_payment_gateways', ['PayWave\\Gateway', 'add_gateway_class']);
-    }
-}
